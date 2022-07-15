b9f4eae1d6aff97fd2f8f1523cd2e9d7127f37c4
elvis <cenelvis@gmail.com>
2022-07-15 12:07:18 -0400

    Types Basics

---
+++ basics.ts
@@ -0,0 +1,45 @@
+// Primitives: number, string, boolean
+// More complex types: arrays, objects
+// Function types, parameters
+
+// Primitives
+
+let age: number;
+
+age = 12;
+
+let userName: string;
+
+userName = "Max";
+
+let isInstructor: boolean;
+
+isInstructor = true;
+
+// More complex types
+
+// Array of Strings
+let hobbies: string[];
+
+hobbies = ["Sports", "Cooking"];
+
+// Object Type
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Max",
+  age: 32,
+};
+
+// person = {
+//   isEmployee: true
+// };
+
+// Array of Object types
+let people: {
+  name: string;
+  age: number;
+}[];
