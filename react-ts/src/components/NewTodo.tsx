4ce003809f4e14f9778f7264823a94d6a848de02
elvis <cenelvis@gmail.com>
2022-07-17 19:29:33 -0400

    Working with Refs & useRef, useRef<HTMLInputElement>(null), (?.value, !.value)

--- react-ts/src/components/NewTodo.tsx
+++ react-ts/src/components/NewTodo.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { useRef } from "react";
 
 // hrmlFor="text" => CONNECTS => id="text"
 const NewTodo = () => {
+  // <HTMLInputElement> => Type for HTML Input Elements
+  const todoTextInputRef = useRef<HTMLInputElement>(null); // Requires Default Starting Value (null) because no value
+
+  // TypeScript expects/automatically add this
+  // ?.value => string | undefined
+  // !.value => string (use if 100% sure we have value)
+  const enteredText = todoTextInputRef.current!.value;
+
+  if (enteredText.trim().length === 0) {
+    // throw err
+    return;
+  }
+
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
   };
@@ -9,7 +23,7 @@ const NewTodo = () => {
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor="text">Todo text</label>
-      <input type="text" id="text" />
+      <input type="text" id="text" ref={todoTextInputRef} />
       <button>Add Todo</button>
     </form>
   );
